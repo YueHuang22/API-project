2d6a29f53af5af5daf856d659fb736eda70254b6
Yue Huang <wongyue22@gmail.com>
2022-06-29 13:45:26 -0400

    Add an API router and Add error-handling middleware for common errors and debugging

---
+++ backend/routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const apiRouter = require('./api');
+
+const router = express.Router();
+router.use('/api', apiRouter);
+
+// router.get('/hello/world', function (req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
+
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    });
+});
+
+module.exports = router;
