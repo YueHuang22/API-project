2762518600ed08bcc18fe616e6c7fbe68da40157
Yue Huang <wongyue22@gmail.com>
2022-07-22 14:17:32 -0400

    login thunk

---
+++ frontend/src/store/session.js
@@ -0,0 +1,61 @@
+// {
+//     user: {
+//         id,
+//             email,
+//             username,
+//             createdAt,
+//             updatedAt
+//     }
+// }
+
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER,
+    };
+};
+
+export const login = (user) => async (dispatch) => {
+    const { email, password } = user;
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
