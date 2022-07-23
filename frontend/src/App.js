d46633305d1d9c88dc7f6bac3f627b153c960abd
Yue Huang <wongyue22@gmail.com>
2022-07-23 13:15:26 -0400

    fix restore user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
-import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import * as sessionActions from "./store/session";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+    dispatch(sessionActions.restoreUser()).then(() =>
+      setIsLoaded(true));
   }, [dispatch]);
 
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
+import * as groupActions from "./store/groups";
+import * as eventActions from "./store/events";
 
 const store = configureStore();
 
@@ -16,6 +18,9 @@ if (process.env.NODE_ENV !== 'production') {
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
+  window.sessionActions = sessionActions;
+  window.groupActions = groupActions;
+  window.eventActions = eventActions;
 }
 
 function Root() {
